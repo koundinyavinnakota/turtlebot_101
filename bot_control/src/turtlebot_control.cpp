a889be96bc8281bee99428a48c6c14f408be5598
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-12-06 22:32:07 -0500

    Doxygen documentation

--- bot_control/src/turtlebot_control.cpp
+++ bot_control/src/turtlebot_control.cpp
@@ -1,7 +1,7 @@
 /**
  * @file publish_details.cpp
  * @author Koundinya Vinnakota
- * @brief This the publisher and servie provider
+ * @brief This the publisher and subscriber that controls the turtlebot
  * @version 0.1
  * @date 2022-11-18
  *
@@ -10,11 +10,11 @@
  */
 
 #include <chrono>
-#include <memory>
-#include "rclcpp/rclcpp.hpp"
 #include <geometry_msgs/msg/twist.hpp>
+#include <memory>
 #include <sensor_msgs/msg/laser_scan.hpp>
-// #include "std_msgs/msg/LaserScan.hpp"
+
+#include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -28,68 +28,70 @@ class bot_control : public rclcpp::Node {
    * @brief Construct a new Details Publisher object
    *
    */
-  bot_control() : Node("bot_control") 
-  {
+  bot_control() : Node("bot_control") {
     // subscription to the laser scan topic
     subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-      "/scan", 1, std::bind(&bot_control::topic_callback, this, _1));
-    
+        "/scan", 1, std::bind(&bot_control::topic_callback, this, _1));
 
-    //Creating a publisher for bot control
-    botPublisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
-   
-    RCLCPP_INFO(this->get_logger(),"Obstacle flag : %d",thresholdCroseed);
-  
+    // Creating a publisher for bot control
+    botPublisher_ =
+        this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
+
+    RCLCPP_INFO(this->get_logger(), "Obstacle flag : %d", thresholdCroseed);
   }
+
  private:
- 
-  void topic_callback(const sensor_msgs::msg::LaserScan::ConstPtr & intensities)
-  {
-    detector(intensities);
+ /**
+  * @brief This is a subscriber call back function
+  * 
+  * @param intensities 
+  */
+  void topic_callback(
+      const sensor_msgs::msg::LaserScan::ConstPtr &intensities) {
+    
+    detector(intensities);// function call for detector alogorithm
     auto message = geometry_msgs::msg::Twist();
-    if(thresholdCroseed){
-      RCLCPP_INFO(this->get_logger(),"Danger turn around");
+    if (thresholdCroseed) {
+      RCLCPP_INFO(this->get_logger(), "Danger turn around");
       message.linear.x = 0.0;
       message.angular.z = rotateAngle;
-      
-    }else{
-      RCLCPP_INFO(this->get_logger(),"Safe to move forward");
+
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Safe to move forward");
       message.linear.x = 0.4;
       message.angular.z = 0.0;
-      
     }
-    botPublisher_->publish(message);
+    botPublisher_->publish(message);// publishing to /cmd_vel
   }
-
-  void detector(const sensor_msgs::msg::LaserScan::ConstPtr & intensities){
+  /**
+   * @brief This is a function that detects obstacles
+   * 
+   * @param intensities 
+   */
+  void detector(const sensor_msgs::msg::LaserScan::ConstPtr &intensities) {
     for (auto intensity : intensities->ranges) {
       if (intensity <= thresholdIntensity) {
         thresholdCroseed = true;  /// changes flag if threshold crossed
-        RCLCPP_INFO(this->get_logger(),"Danger");
+        RCLCPP_INFO(this->get_logger(), "Danger");
         break;
       } else {
         thresholdCroseed = false;
         break;
       }
-
     }
   }
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr botPublisher_;
   rclcpp::TimerBase::SharedPtr timer_;
   geometry_msgs::msg::Twist mover_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
-  double thresholdIntensity = 0.9;
-  double rotateAngle = 0.7;
-  bool thresholdCroseed = false;
-
-  
+  double thresholdIntensity = 0.9;// Threshold intensity - distance proximity
+  double rotateAngle = 0.7;// Amount of rotation of turtlebot
+  bool thresholdCroseed = false;// obstacle flag
 };
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<bot_control>());
   rclcpp::shutdown();
   return 0;
 }
-
